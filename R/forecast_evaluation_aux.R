863bcb559badc908254818821dc2b385c9603fa6
aciancetta <alessandro.ciancetta@gmail.com>
2022-08-12 22:55:05 +0200

    Adding forecast_evaluation_aux

---
+++ R/forecast_evaluation_aux.R
@@ -0,0 +1,33 @@
+#' Wrapper for forecast evaluation
+#'
+#' @param type String to choose the algorithm. Either "PCA", "KF", "KS", "EM"
+#' @param evaluation_input A list
+#'
+#' @return See `forecast_evaluation`
+#' @export
+#' @seealso `forecast_evaluation`
+#'
+#' @examples
+#' # d <- download_clean_data("IT")
+#' # evaluation_input <- list(d = d,
+#' #                          horizon = 1,
+#' #                          window_size = 300
+#' #                          r = 4,
+#' #                          p = 1,
+#' #                          thresh_imputation = 0.2,
+#' #                          thresh = 0.01                 # L1 convergence of parameters in EM
+#' #                          )
+#' #
+#' # forecast_eval <- forecast_evaluation_aux("EM", evaluation_input)
+#' # plot_forecast_evaluation(forecast_eval)
+#'
+forecast_evaluation_aux <- function(type, evaluation_input){
+  forecast_evaluation(type = type,
+                      d = evaluation_input$d,
+                      horizon = evaluation_input$horizon,
+                      window_size = evaluation_input$window_size,
+                      r = evaluation_input$r,
+                      p = evaluation_input$p,
+                      thresh_imputation = evaluation_input$thresh_imputation,
+                      thresh = evaluation_input$thresh)
+}
